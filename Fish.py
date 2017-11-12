3d0c3e52b460c5511da67e67b0080890066cafa0
Isak Hjortgren <isakhjortgren@Isaks-MacBook-Pro.local>
2017-11-12 14:32:01 +0100

    update shell

--- Fish.py
+++ Fish.py
@@ -1,5 +1,6 @@
 import numpy as np
 
 class Fish(object):
-    def __init__(self):
+    def __init__(self, nbr_of_inputs, nbr_of_outputs, nbr_of):
+        # ToDO: write the fish brain.
         pass
