3c29e7351477fb20e978ab3ef8d231bc23c27852
Isak Hjortgren <isakhjortgren@Isaks-MacBook-Pro.local>
2017-11-14 19:31:03 +0100

    update brain function for acceleration

--- Brain.py
+++ Brain.py
@@ -2,16 +2,20 @@ import numpy as np
 
 
 class Brain(object):
-    def __init__(self, nbr_of_hidden_neurons, nbr_of_inputs=8, nbr_of_outputs=2):
+    def __init__(self, nbr_of_hidden_neurons, nbr_of_inputs=8, nbr_of_outputs=2, maximum_acceleration=1):
         self.weights1 = np.random.rand(nbr_of_hidden_neurons, nbr_of_inputs + 1)
         self.weights2 = np.random.rand(nbr_of_outputs, nbr_of_hidden_neurons + 1)
+        self.maximum_acceleration = maximum_acceleration
 
     def make_decision(self, mean_prey_pos, mean_predator_pos, mean_prey_vel, mean_predator_vel):
         total_inputs = np.concatenate((mean_prey_pos, mean_predator_pos, mean_prey_vel, mean_predator_vel, [1]))
         hidden_state = np.tanh(np.dot(self.weights1, total_inputs))
         output_state = np.tanh(np.dot(self.weights2, hidden_state))
+        return output_state
+
+    def get_acceleration(self):
+        output_state = self.make_decision()
         norm = np.linalg.norm(output_state)
         if norm > 1:
             output_state = output_state/norm
-        return output_state
-
+        return self.maximum_acceleration * output_state
