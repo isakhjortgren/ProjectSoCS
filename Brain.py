6e50fec6e0ae83d9692541f8a942b88e9beba9c8
Andreas Grigoriadis <andgrig@ft4011-09.ft4011.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-11-17 08:48:31 +0100

    Added brain update function

--- Brain.py
+++ Brain.py
@@ -2,11 +2,13 @@ import numpy as np
 
 
 class Brain(object):
-    def __init__(self, nbr_of_hidden_neurons, nbr_of_inputs=10, nbr_of_outputs=2, maximum_acceleration=1,
+    def __init__(self, nbr_of_hidden_neurons, nbr_of_inputs=10, nbr_of_outputs=2,
                  weight_range=5):
+        self.nbr_of_hidden_neurons=nbr_of_hidden_neurons
+        self.nbr_of_inputs=nbr_of_inputs
+        self.nbr_of_outputs=nbr_of_outputs
         self.weights1 = weight_range*(2*np.random.rand(nbr_of_hidden_neurons, nbr_of_inputs + 1) - 1)
         self.weights2 = weight_range*(2*np.random.rand(nbr_of_outputs, nbr_of_hidden_neurons + 1) - 1)
-        self.maximum_acceleration = maximum_acceleration
 
     def make_decision(self, mean_prey_pos, mean_predator_pos, mean_prey_vel, mean_predator_vel, rel_pos):
         total_inputs = np.concatenate((mean_prey_pos, mean_predator_pos, mean_prey_vel, mean_predator_vel, rel_pos, [1]))
@@ -18,4 +20,8 @@ class Brain(object):
             output_state = output_state / norm
         return output_state
 
+    def update_brain(self, weight_array):
+        number_weights1 = self.nbr_of_hidden_neurons*(self.nbr_of_inputs + 1)
+        self.weights1 = weight_array[0: number_weights1].reshape(self.nbr_of_hidden_neurons, self.nbr_of_inputs + 1)
+        self.weights2 = weight_array[number_weights1:].reshape(self.nbr_of_outputs, self.nbr_of_hidden_neurons + 1)
 #ToDo Add function that sets weight with given input weights vector
