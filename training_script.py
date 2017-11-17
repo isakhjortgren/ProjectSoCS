d818edc472c9c413828f43049e93cd67bca05bfc
Isak Hjortgren <isakhjortgren@Isaks-MacBook-Pro.local>
2017-11-17 10:20:51 +0100

    update training script

--- training_script.py
+++ training_script.py
@@ -1,9 +1,16 @@
 import pickle
 from PSO_class import PSO
+from Aquarium import aquarium
 
 pso = PSO(train_prey=False)
 
 pso.run_pso()
 
+aquarium_1 = aquarium(pso.aquarium_parameters)
+aquarium_1.pred_brain.update_brain(pso.swarm_best_position)
+
+
+
 with open('TrainingData.p', 'wb') as f:
     pickle.dump(pso, f)
+
