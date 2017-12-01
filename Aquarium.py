b5af73999fe5fd9b1f7f37d5ddc3b8b527fab0bc
Isak Hjortgren <isakhjortgren@Isaks-MacBook-Pro.local>
2017-12-01 09:06:09 +0100

    update maxtime

--- Aquarium.py
+++ Aquarium.py
@@ -350,7 +350,7 @@ class aquarium(object):
         dt = 0.25*  min(self.eat_radius, self.collision_len) / max(self.max_vel_prey, self.max_vel_pred)
         time = 0
 
-        MAX_TIME = 50
+        MAX_TIME = 100
         HALF_NBR_FISHES = len(self.fish_xy_start) // 2
 
         self.fish_xy = np.copy(self.fish_xy_start )
