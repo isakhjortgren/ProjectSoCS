a60c1c03372dca5cc772a760775126df2c46b5b5
Isak Hjortgren <isakhjortgren@Isaks-MBP.lan>
2017-12-01 19:38:11 +0100

    changed activation function in last layer

--- Brain.py
+++ Brain.py
@@ -14,10 +14,8 @@ class Brain(object):
         total_inputs = np.concatenate((total_inputs, [1]))
         hidden_state = np.tanh(np.dot(self.weights1, total_inputs))
         hidden_state = np.concatenate((hidden_state, [1]))
-        output_state = np.tanh(np.dot(self.weights2, hidden_state))
-        norm = np.linalg.norm(output_state)
-        if norm > 1:
-            output_state = output_state / norm
+        output_state = (np.dot(self.weights2, hidden_state))
+
         return output_state
 
     def update_brain(self, weight_array):
@@ -29,8 +27,36 @@ class Brain(object):
 
 class randomBrain(Brain):
     def make_decision(self, total_inputs):
-        return (np.random.random((1,2))-0.5)*2
+        return (np.random.random((1, 2))-0.5)*2
 
     def update_brain(self, weight_array):
         raise NotImplementedError('Do not fucking train random walk sharks!!!')
+
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    nbr_input = 6
+    hn = 2
+    wr = 1
+    runs = 100000
+    nbr_out = 2
+    data = np.zeros((runs, 2))
+    try:
+        for i in range(runs):
+            rand_input = 2*np.random.rand(nbr_input)-1
+            tmp_brain = Brain(nbr_of_hidden_neurons=hn, nbr_of_inputs=nbr_input, weight_range=wr, nbr_of_outputs=nbr_out)
+            tmp_out = tmp_brain.make_decision(rand_input)
+            if nbr_out == 2:
+                data[i, :] = tmp_out
+            elif nbr_out == 4:
+                data[i, :] = [tmp_out[0]+tmp_out[1], tmp_out[2]+tmp_out[3]]
+    except:
+        print('ouch')
+    plt.hist2d(data[:,0], data[:,1],bins=50)
+    plt.colorbar()
+    plt.show()
+
+
+
 
