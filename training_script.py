940b4fe1c3f482ac9bc51bd40dc8cf5de635be4a
Isak Hjortgren <isakhjortgren@Isaks-MacBook-Pro.local>
2017-12-08 13:50:33 +0100

    update training_script so that it asks if one shall continue on previous training or start a new one. The script do also save data in every alternation.

--- training_script.py
+++ training_script.py
@@ -1,4 +1,5 @@
 import pickle
+import os
 from PSO_class import PSO
 import traceback
 from Aquarium import aquarium
@@ -13,6 +14,28 @@ aquarium_parameters = {'nbr_of_prey': 20, 'nbr_of_pred': 3, 'size_X': 2, 'size_Y
 
 list_of_pso_prey = list()
 list_of_pso_pred = list()
+start_iteration = 0
+if os.path.isfile('TrainingData.p'):
+    var = str(input("Continue training? (y/n): "))
+
+    if var == 'y':
+        print('Continue training!')
+        with open('TrainingData.p', 'rb') as f:
+            pso_data = pickle.load(f)
+        list_of_pso_prey = pso_data['list_of_pso_prey']
+        list_of_pso_pred = pso_data['list_of_pso_pred']
+        start_iteration = len(list_of_pso_pred)
+        aquarium_parameters = list_of_pso_pred[-1].aquarium_parameters
+        print('current aquarium parameters:\n', aquarium_parameters)
+        best_pred_brain = list_of_pso_pred[-1].swarm_best_position
+        best_prey_brain = list_of_pso_prey[-1].swarm_best_position
+
+        if start_iteration == 0:
+            raise RuntimeError('No data in TrainingData.p file use new one!')
+    elif var == 'n':
+        print('Starting new training!')
+    else:
+        raise Exception('abort training, wrong input')
 
 prey_iterations = 20
 pred_iterations = 20
@@ -23,8 +46,8 @@ nbr_of_training_alternations = 2 * total_iteraions // (pred_iterations + prey_it
 
 
 try:
-    for i in range(nbr_of_training_alternations):
-        print('Training preys, iteration: ', i+1, ' out of ', nbr_of_training_alternations)
+    for i in range(start_iteration, nbr_of_training_alternations):
+        print('Training preys, alternation: ', i+1, ' out of ', nbr_of_training_alternations)
         # Train prey
         if i != 0:
             pso_prey = PSO(aquarium_parameters=aquarium_parameters, train_prey=True)
@@ -54,7 +77,7 @@ try:
 
         list_of_pso_prey.append(pso_prey)
 
-        print('Training predators, iteration: ', i+1, ' out of ', nbr_of_training_alternations)
+        print('Training predators, alternation: ', i+1, ' out of ', nbr_of_training_alternations)
         # Train predator
         pso_pred = PSO(aquarium_parameters=aquarium_parameters, train_prey=False)
         pso_pred.nbr_of_iterations = pred_iterations
@@ -79,6 +102,13 @@ try:
 
 
         list_of_pso_pred.append(pso_pred)
+
+        pso_data = {'list_of_pso_pred': list_of_pso_pred, 'list_of_pso_prey': list_of_pso_prey}
+        with open('TrainingData.p', 'wb') as f:
+            pickle.dump(pso_data, f)
+            print('data saved!')
+
+
 except KeyboardInterrupt:
     print('Training aborted for some reason')
 except RuntimeError:
